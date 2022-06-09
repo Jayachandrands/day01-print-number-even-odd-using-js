2ff15039dc234e3c5ee9d63f56cd7f3f49eef7e9
jayachandrands <jaidurai1422@gmail.com>
2022-06-09 07:45:47 -0700

    task1

---
+++ printevenodd.js
@@ -0,0 +1,9 @@
+for(let x=0;x<=25;x++){
+    if(x==0){
+        console.log(x + "is zero");
+    }else if(x%2==0){
+        console.log(x + "is even");
+    }else{
+        console.log(x + "is odd");
+    }
+}
